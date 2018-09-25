0e37ab13bb0ec32a2e3e38fe941c6074d99122bc
PierreFou <pierrefou17@gmail.com>
2018-09-25 17:19:43 +0200

    Initial Commit

---
+++ MobiApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MobiApps
+//
+//  Created by Pierre on 25/09/2018.
+//  Copyright © 2018 Pierre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
